e3c751ab68b7dee3c649a260f2e6a1ddf9b8e0e7
apknation <atanasikafuka@gmail.com>
2026-07-31 17:05:49 +0300

    update

---
+++ src/app/components/betting-platforms/betting-platforms.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-betting-platforms',
+  imports: [],
+  templateUrl: './betting-platforms.html',
+  styleUrl: './betting-platforms.css',
+})
+export class BettingPlatforms {}
---
+++ src/app/components/vip-room/vip-room.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vip-room',
+  imports: [],
+  templateUrl: './vip-room.html',
+  styleUrl: './vip-room.css',
+})
+export class VipRoom {}
