8d34def5c8f5c6dc5d43cb5535c29d3944edd7ba
apknation <atanasikafuka@gmail.com>
2026-07-31 17:11:01 +0300

    update

--- src/app/components/payments/payments.ts
+++ src/app/components/payments/payments.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-payments',
   imports: [],
   templateUrl: './payments.html',
-  styleUrl: './payments.css',
+  styleUrl: './payments.css'
 })
-export class Payments {}
+export class Payments {
+  paymentMethods = [
+    { name: 'M-Pesa', color: 'bg-green-500' },
+    { name: 'Airtel Money', color: 'bg-red-500' },
+    { name: 'Halotel (HaloPesa)', color: 'bg-orange-500' }
+  ];
+  
+  transactionType = signal<'deposit' | 'withdraw'>('deposit');
+
+  setTransactionType(type: 'deposit' | 'withdraw') {
+    this.transactionType.set(type);
+  }
+}
